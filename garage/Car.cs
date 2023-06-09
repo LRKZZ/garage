96d9f02d09cae1cd1ce25c52937fc45950292b15
ugly code enjoyer <81513533+LRKZZ@users.noreply.github.com>
2023-06-09 10:01:11 +0300

    Update Car.cs

--- garage/Car.cs
+++ garage/Car.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace garage
 {
-    internal class Car
+    class Car
     {
+        string name;
+        int id;
+        public Car(string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+        public string Name { get { return name; } }
+        public int Id { get { return id; } }
     }
 }
