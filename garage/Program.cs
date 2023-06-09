0db2a317e8aa0c4249c3b4c5866bfb8844a41f17
ugly code enjoyer <81513533+LRKZZ@users.noreply.github.com>
2023-06-09 10:01:41 +0300

    Update Program.cs

--- garage/Program.cs
+++ garage/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace garage
+{
+    delegate void Washer(Car car);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во машин");
+            int n = Int32.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>(n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введите название");
+                string name = Console.ReadLine();
+                Console.WriteLine("Введите id машины");
+                int id = Int32.Parse(Console.ReadLine());
+                cars.Add(new Car(name, id));
+            }
+            Garage garage = new Garage(cars);
+
+            Wash wash = new Wash();
+            Washer washi = new Washer(wash.WashCar);
+
+
+            foreach (Car car in garage.Cars)
+            {
+
+                washi(car);
+            }
+            Console.ReadKey();
+        }
+    }
+}
